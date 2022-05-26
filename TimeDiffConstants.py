5d10cab8d1afe251e2a58f156a964f18a2d9bf5d
milosz-l <milosz.lopatto@gmail.com>
2022-05-26 12:17:03 +0200

    added new data and TimeDiffDataTransformer

---
+++ TimeDiffConstants.py
@@ -0,0 +1,15 @@
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+PRICE_TRESHOLD = 100_000    # for outliers
+WEIGHT_TRESHOLD = 50        # for outliers
+NUM_OF_HOURS = 12
+SEED = 42
+
+COLS_TO_DROP_ALWAYS = ["delivery_timestamp",
+                       "session_id",
+                       "purchase_id",
+                       "event_type",
+                       "name",
+                       "user_id",
+                       'offered_discount',
+                       'optional_attributes',
+                       'purchase_timestamp']
