010a677c85bc0f3d7cfdd9cfe8c32a607e4b0e7b
Fran3x <franex@onet.eu>
2022-05-30 12:37:55 +0200

    model_training converted

--- frontend.py
+++ frontend.py
@@ -3,6 +3,10 @@ import time
 import numpy as np
 import datetime
 
+def getPrediction():
+     print("get prediction")
+
+
 status_text = st.sidebar.empty()
 
 d_start = st.date_input("Date of purchase")
@@ -11,9 +15,9 @@ t_start = st.time_input('Time of purchase')
 
 model = st.selectbox(
      'Choose model',
-     ('Basic model', 'Advanced model'))
+     ('Basic model (linear regression)', 'Advanced model (random forest)'))
 
-submit = st.button("Get prediction", on_click=None)
+submit = st.button("Get prediction", on_click=getPrediction)
 
 time = 0
 
