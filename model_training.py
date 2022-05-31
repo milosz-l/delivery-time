e2426c09925ae40f20715e8948406b60263623e9
Fran3x <franex@onet.eu>
2022-05-31 22:26:50 +0200

    example json adjusted

--- model_training.py
+++ model_training.py
@@ -1,38 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
 from TimeDiffDataTransformer import TimeDiffDataTransformer
 from TimeDiffConstants import DATE_FORMAT, PRICE_TRESHOLD, WEIGHT_TRESHOLD, NUM_OF_HOURS, SEED, COLS_TO_DROP_ALWAYS
 
-
-# In[2]:
-
-
-# warnings.filterwarnings('ignore')
-
-
-# # preparing data
-
-# In[3]:
-
-
-time_diff_data = TimeDiffDataTransformer()
-time_diff_data.make_all_transformations()
-df = time_diff_data.get_df()
-
-
-# # training model
-
-# In[4]:
-
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 
 def split_data(df, target_column="time_diff"):
     y = df["time_diff"].to_numpy()
@@ -40,16 +14,10 @@ def split_data(df, target_column="time_diff"):
     return train_test_split(X, y, test_size=0.2, random_state=SEED)
 
 
-# In[5]:
-
-
 def train_models(models_list, X_train, y_train):
     for model in models_list:
         model.fit(X_train, y_train)
     return models_list
-
-
-# In[6]:
 
 
 def create_df_with_predictions(models_list, X_test, y_test):
@@ -60,25 +28,16 @@ def create_df_with_predictions(models_list, X_test, y_test):
     return y_pred_df
 
 
-# In[7]:
-
-
 def display_predictions(y_pred_df):
     display(y_pred_df.head())
     display(y_pred_df.info())
     display(y_pred_df.describe())
 
 
-# In[8]:
-
-
 def print_scores(models_list, X_test, y_test):
     for model in models_list:
         score = model.score(X_test, y_test)
         print(f"{type(model).__name__} score = {score}")
-
-
-# In[9]:
 
 
 def print_percent_of_good_predictions(models_list, X_test, y_test, error=NUM_OF_HOURS*60*60):
@@ -91,32 +50,39 @@ def print_percent_of_good_predictions(models_list, X_test, y_test, error=NUM_OF_
         print(f'which is {percent_of_good_predictions * 100}%')
 
 
-# In[10]:
-
-
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 
 
+# # preparing data
+
+time_diff_data = TimeDiffDataTransformer()
+
+
+print("DF", df)
+time_diff_data.make_all_transformations()
+df = time_diff_data.get_df()
+
 X_train, X_test, y_train, y_test = split_data(df)
 
+# # models
 models_list = [Ridge(alpha=0.1),
                Lasso(alpha=0.1),
                DecisionTreeRegressor(random_state=SEED),
                RandomForestRegressor(random_state=SEED)]
+
+# # training model
 models_list = train_models(models_list, X_train, y_train)
+
 
 y_pred_df = create_df_with_predictions(models_list, X_test, y_test)
 # display_predictions(y_pred_df)
 
+
+# # printing results
 print_scores(models_list, X_test, y_test)
 
 print_percent_of_good_predictions(models_list, X_test, y_test)
 
-
-# In[11]:
-
-
 print_percent_of_good_predictions(models_list, X_test, y_test, error=NUM_OF_HOURS/2*60*60)
-
