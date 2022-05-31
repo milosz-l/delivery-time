a375589b3f56755ae7346a044675da0c05b16f44
Fran3x <franex@onet.eu>
2022-05-31 22:26:50 +0200

    example json adjusted

--- microservice/app.py
+++ microservice/app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
     print("NEW POST REQUEST")
-    print("D", request.data)  # raw data
-    print("J", request.json)  # json (if content-type of application/json is sent with the request)
-    print(request.get_json(force=True))  # json (if content-type of application/json is not sent)
+    data = request.json
+    print("DATA", data)  # json (if content-type of application/json is sent with the request)
     return "OK"
