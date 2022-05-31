826fc45e0afbd9a5d9c0fd3fdf038f8e955223c1
Fran3x <franex@onet.eu>
2022-06-01 01:52:17 +0200

    data from post request preprocessed

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request
 from model_training import *
+import pandas as pd
 
 app = Flask(__name__)
 
 models = getTrainedModels()
+
+from TimeDiffDataTransformer import *
+time_diff_data = TimeDiffDataTransformer()
+
+time_diff_data.make_all_transformations()
+df = time_diff_data.get_df()
+
 print(models)
+
+def preprocessData(data):
+    df = pd.DataFrame(data)
+    df["purchase_datetime_delta"] = 0
+    df["time_diff"] = 0
+    df["delivery_timestamp"] = 0
+    return df
 
 @app.route('/', methods=['POST'])
 def result():
@@ -12,5 +27,10 @@ def result():
     data = request.json
     print("DATA", data)  # json (if content-type of application/json is sent with the request)
 
+    df = preprocessData(data)
+    print(df.keys())
+
+    # prediction_base_model = models[0].predict(data)
+    # print(prediction_base_model)
 
     return "OK"
