55576aa1479036bfc9b18fcdf699d01788c05346
Fran3x <franex@onet.eu>
2022-06-03 23:54:16 +0200

    test size = 0.001

--- TimeDiffConstants.py
+++ TimeDiffConstants.py
@@ -6,7 +6,7 @@ PRICE_TRESHOLD = 100_000    # for outliers
 WEIGHT_TRESHOLD = 50        # for outliers
 NUM_OF_HOURS = 24
 SEED = randint(0, 100000)  # 42
-TEST_SIZE = 0.2
+TEST_SIZE = 0.001
 
 COLS_TO_DROP_ALWAYS = ("delivery_timestamp",
                        "session_id",
